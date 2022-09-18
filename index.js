49409910dac9ad9a5a00309acf72c63db733ad1e
sunylexmc <113857021+sunylexmc@users.noreply.github.com>
2022-09-18 16:33:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -60,9 +60,6 @@ function modifyState() {
 function notInPoint() {
     alert("This feature is still not in point!");
 }
-if (maintenance === true) {
-    window.location.href = "./Maintenance Web/index.html";
-}
 document.getElementById("navbar1").innerHTML = `
 <li><a onclick="modifyState()">Home</a></li>
 <li><a href="`+applyLink+`">Apply</a></li>
